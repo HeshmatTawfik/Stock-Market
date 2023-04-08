e733e6c808ed987ef9e9f8dc9ec25d7700270a50
heshmattawfik <heshmattawfik@gmail.com>
2023-04-08 16:22:37 +0200

    test staging

--- app/src/main/java/com/heshmat/stockmarket/MainActivity.kt
+++ app/src/main/java/com/heshmat/stockmarket/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             //testbitrise
+            // test staging 1.0.0
             StockMarketTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
